726bd830b35cbb964fb78fe0d33267304889d377
bhargav-siddanati <bhargav77990@gmail.com>
2025-11-22 17:27:28 +0530

    add spring data jpa, modified the application file

---
+++ src/main/java/com/ecomm/app/UserRespository.java
@@ -0,0 +1,7 @@
+package com.ecomm.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRespository extends JpaRepository<User, Long> {}
--- src/main/java/com/ecomm/app/UserService.java
+++ src/main/java/com/ecomm/app/UserService.java
@@ -1,37 +1,34 @@
 package com.ecomm.app;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-  private List<User> userList = new ArrayList<>();
-  private Long increID = 1L;
-
+  private final UserRespository userRespository;
   public List<User> fetchUsers() {
-    return userList;
+    return userRespository.findAll();
   }
 
   public Optional<User> getUser(Long id) {
-    return userList.stream().filter(user -> user.getId().equals(id))
-            .findAny();
+    return userRespository.findById(id);
   }
 
   public String createUser(User user) {
-    user.setId(increID++);
-    userList.add(user);
+    userRespository.save(user);
     return "User added successfully";
   }
 
   public boolean updateExistedUser(Long id, User user){
-    return userList.stream().filter(u -> u.getId().equals(id))
-            .findAny()
+    return userRespository.findById(id)
             .map(m -> {
               m.setLastName(user.getLastName());
               m.setFirstName(user.getFirstName());
+              userRespository.save(m);
               return true;
             }).orElse(false);
   }
